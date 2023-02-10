395f17d9dbe7180ccf69ca71f30800c9c201e16b
Alfio Ferrara <alfio.ferrara@unimi.it>
2023-02-10 08:41:37 +0100

    Materials for class 2, 2023

--- rla/algorithms.py
+++ rla/algorithms.py
@@ -18,28 +18,27 @@ def Qlearning(mdp,
     history = []
     total_reward_per_episode = []
     for episode in range(num_episodes):
-        R = 0
-        Q_old = copy.deepcopy(Q)
+        R = defaultdict(lambda : 0)
         state = np.random.choice(list(mdp.start()))
         for step in range(max_steps):
+            history.append(copy.deepcopy(Q))
             action = np.random.choice(list(mdp.actions(state))) # random policy (try greedy)
             # Take action
             successors = [x for x in mdp.successors(state, action)]
             if len(successors) > 0:
                 p = [p for _, p, _ in successors]
                 s_prime, _, reward = successors[np.random.choice(range(len(successors)), p=p)]
-                R += reward
+                R[action] += reward
                 # Update
-                Q[state][action] = (1-eta) * Q_old[state][action] + eta * (
-                    reward + mdp.gamma * max(Q_old[s_prime].values())
+                Q[state][action] = (1-eta) * Q[state][action] + eta * (
+                    reward + mdp.gamma * max(Q[s_prime].values())
                 )
                 state = s_prime
                 if mdp.end(state):
                     break
             else:
                 break
-        history.append(copy.deepcopy(Q_old))
-        total_reward_per_episode.append(R)
+        total_reward_per_episode.append(copy.deepcopy(R))
 #        diff = 0
 #        for k, v in Q.items():
 #            for x, y in v.items():
@@ -64,39 +63,41 @@ def sarsa(mdp,
     N = dict([(s, 1) for s in mdp.states()])
 
     # Set up functions for choosing the policy
-    def epsilon(state):
-        return 1 / N[state]
+    def epsilon(s_check):
+        return 1 / N[s_check]
 
-    def epsilon_greedy(state):
+    def epsilon_greedy(s_check):
         x = np.random.uniform()
-        if x < epsilon(state):
-            action = np.random.choice(list(mdp.actions(state)))
+        if x < epsilon(s_check):
+            to_action = np.random.choice(list(mdp.actions(s_check)))
         else:
-            action = max([(v, a) for a, v in Q[state].items()])[1]
-        return action
+            to_action = max([(v, a) for a, v in Q[s_check].items()])[1]
+        return to_action
 
     # set up a history
     history = []
     total_reward_per_episode = []
     for episode in range(num_episodes):
-        R = 0
-        Q_old = copy.deepcopy(Q)
+        R = defaultdict(lambda : 0)
         state = np.random.choice(list(mdp.start()))
         action = epsilon_greedy(state)
         for step in range(max_steps):
+
+            history.append(copy.deepcopy(Q))
+
             # Take action
             successors = [x for x in mdp.successors(state, action)]
             if len(successors) > 0:
                 p = [p for _, p, _ in successors]
                 s_prime, _, reward = successors[np.random.choice(range(len(successors)), p=p)]
-                R += reward
+                R[action] += reward
 
                 # Now observe what happens if you use your policy on s_prime
                 a_prime = epsilon_greedy(s_prime)
 
                 # Update
-                Q[state][action] = (1-eta) * Q_old[state][action] + eta * (
-                    reward + mdp.gamma * Q_old[s_prime][a_prime]
+                Q[state][action] = (1-eta) * Q[state][action] + eta * (
+                    reward + mdp.gamma * Q[s_prime][a_prime]
                 )
                 state = s_prime
                 action = a_prime
@@ -104,8 +105,7 @@ def sarsa(mdp,
                     break
             else:
                 break
-        history.append(copy.deepcopy(Q_old))
-        total_reward_per_episode.append(R)
+        total_reward_per_episode.append(copy.deepcopy(R))
 #        diff = 0
 #        for k, v in Q.items():
 #            for x, y in v.items():
@@ -113,3 +113,85 @@ def sarsa(mdp,
 #        if diff < min_diff:
 #            break
     return Q, history, total_reward_per_episode
+
+
+def sarsa_lambda(mdp,
+                 lambda_: float = .8,
+                 num_episodes: int = 1000,
+                 max_steps: int = 100,
+                 min_diff: float = 1e-3,
+                 eta: float = 0.01):
+    # Init the Q(S,A) table
+    Q = defaultdict(lambda: defaultdict(lambda: float('-inf')))
+    for state in mdp.states():
+        for action in mdp.actions(state):
+            Q[state][action] = 0
+
+    # Eligibility trace
+    E = defaultdict(lambda: defaultdict(lambda: 0))
+    for state in mdp.states():
+        for action in mdp.actions(state):
+            E[state][action] = 0
+
+    # Counter for visits to the states
+    N = dict([(s, 1) for s in mdp.states()])
+
+    # Set up functions for choosing the policy
+    def epsilon(s_check):
+        return 1 / N[s_check]
+
+    def epsilon_greedy(s_check):
+        x = np.random.uniform()
+        if x < epsilon(s_check):
+            to_action = np.random.choice(list(mdp.actions(s_check)))
+        else:
+            to_action = max([(v, a) for a, v in Q[s_check].items()])[1]
+        return to_action
+
+    # set up a history
+    history = []
+    total_reward_per_episode = []
+    for episode in range(num_episodes):
+        R = defaultdict(lambda : 0)
+        state = np.random.choice(list(mdp.start()))
+        action = epsilon_greedy(state)
+
+        for step in range(max_steps):
+
+            history.append(copy.deepcopy(Q))
+
+            # Take action
+            successors = [x for x in mdp.successors(state, action)]
+            if len(successors) > 0:
+                p = [p for _, p, _ in successors]
+                s_prime, _, reward = successors[np.random.choice(range(len(successors)), p=p)]
+                R[action] += reward
+
+                # Now observe what happens if you use your policy on s_prime
+                a_prime = epsilon_greedy(s_prime)
+
+                # Compute delta
+                delta = reward + mdp.gamma * Q[s_prime][a_prime] - Q[state][action]
+                E[state][action] = E[state][action] + 1
+
+                # Update
+                for state in mdp.states():
+                    for action in mdp.actions(state):
+                        Q[state][action] = Q[state][action] + eta * delta * E[state][action]
+                        E[state][action] = mdp.gamma * lambda_ * E[state][action]
+
+                state = s_prime
+                action = a_prime
+                if mdp.end(state):
+                    break
+            else:
+                break
+        total_reward_per_episode.append(copy.deepcopy(R))
+#        diff = 0
+#        for k, v in Q.items():
+#            for x, y in v.items():
+#                diff += np.abs(y - Q_old[k][x])
+#        if diff < min_diff:
+#            break
+    return Q, history, total_reward_per_episode
+
--- rla/utils.py
+++ rla/utils.py
@@ -1,5 +1,9 @@
+from collections import defaultdict
+
+import numpy as np
 import pandas as pd
 from IPython.core.display import clear_output, display
+from matplotlib import pyplot as plt
 
 
 def show_q_learning(history):
@@ -10,3 +14,40 @@ def show_q_learning(history):
         cmd = input()
         if cmd == 'quit':
             break
+
+
+def history_to_tensor(history):
+    states = list(history[0].keys())
+    actions = list(history[0][states[0]].keys())
+    t = np.zeros((len(history), len(states), len(actions)))
+    for i, h in enumerate(history):
+        for s, state in enumerate(states):
+            for a, action in enumerate(actions):
+                t[i,s,a] = h[state][action]
+    return t, states, actions
+
+
+def plot_stats(history, rewards):
+    tensor, states, actions = history_to_tensor(history)
+    q_actions = {}
+    for i, s in enumerate(states):
+        for j, a in enumerate(actions):
+            q_actions["{}{}".format(s, a)] = tensor[:, i, j]
+    r_actions = defaultdict(list)
+    for r in rewards:
+        for k, v in r.items():
+            r_actions[k].append(v)
+    return q_actions, r_actions
+
+
+def plot_algorithms(history, rewards):
+    q_actions, r_actions = plot_stats(history, rewards)
+    fig, ax = plt.subplots(figsize=(10, 4), ncols=2)
+    for k, v in r_actions.items():
+        ax[0].plot(v, label=k)
+    ax[0].legend()
+    for k, v in q_actions.items():
+        ax[1].plot(v, label=k)
+    ax[1].legend()
+    plt.tight_layout()
+    plt.show()
