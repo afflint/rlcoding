b9113bb91a94da0bfc88bead93822fe646b3f176
Alfio Ferrara <alfio.ferrara@unimi.it>
2024-03-09 23:35:09 +0100

    New lecture notes updates

--- 2023-24/gymp/gymp/mdp/agents.py
+++ 2023-24/gymp/gymp/mdp/agents.py
@@ -166,7 +166,6 @@ class AgentVFA:
             a = self.greedy_policy(state)
         return a
 
-
     def td_learning(self, max_iterations: int = 1_000):
         """TD(0) learning with e-greedy policy
 
@@ -191,6 +190,129 @@ class AgentVFA:
             else:
                 state = s_prime
     
+    def decay_epsilon(self):
+        """Reduce epsilon for e-greedy policies"""
+        self.epsilon = max(self.e_final, self.epsilon - self.e_decay)
+        
+        
+class AgentQFA:
+    """Generic Agent for State Action Value Function Approximation with discrete actions.
+        In this approach, action information is encoded in the x(s, a) function
+    """
+    def __init__(self, n_features: int, environment: gym.Env, learning_rate: float, epsilon: float, 
+                epsilon_decay: float,
+                final_epsilon: float, 
+                gamma: float = .95) -> None:
+        self.size = n_features
+        self.env = environment
+        self.alpha = learning_rate
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.e_decay = epsilon_decay
+        self.e_final = final_epsilon
+        self.training_error = []
+        w_i = np.ones(self.size)
+        self.w = torch.tensor(w_i, dtype=float, requires_grad=True)
+    
+    @abstractmethod
+    def x(self, state: Any, action: Any) -> torch.Tensor:
+        """Returns the features observing the state and the action
+
+        Args:
+            state (Any): Any Gym state
+            action (Any): Any Gym action
+
+        Returns:
+            torch.Tensor: Feature representation of the array
+        """
+    
+    def q(self, state: Any, action: Any) -> torch.Tensor:
+        """Approximated State-Action Value Function
+
+        Args:
+            state (Any): Any Gym State
+            action (Any): Any Gym Action
+
+        Returns:
+            torch.Tensor: State-Action Value
+        """
+        return self.x(state=state, action=action) @ self.w
+    
+    def greedy_policy(self, state: Any) -> int:
+        """Gets the action leading to the best state
+
+        Args:
+            state (Any): Current state
+
+        Returns:
+            int: action chosen
+        """
+        return np.argmax(self.q(state, action).item() for action in range(self.env.action_space.n)) # assuming discrete action space
+    
+    def e_greedy_policy(self, state: Any) -> int:
+        """Provides epsilon-greedy policy
+
+        Args:
+            state (Any): The observed state
+
+        Returns:
+            int: action
+        """
+        if np.random.random() < self.epsilon:
+            a = self.env.action_space.sample()
+        else:
+            a = self.greedy_policy(state)
+        return a
+    
+    def q_learning(self, max_iterations: int = 1_000):
+        """Performs QLearning training
+
+        Args:
+            max_iterations (int, optional): Maximum number of iterations. Defaults to 1_000.
+        """
+        state, _ = self.env.reset()
+        for i in tqdm(range(max_iterations)):
+            action = self.e_greedy_policy(state)
+            s_prime, reward, terminated, truncated, _ = self.env.step(action)
+            target = reward + self.gamma * max(self.q(s_prime, a_prime).item() for a_prime in range(self.env.action_space.n))
+            q_hat = self.q(state, action)
+            delta = target - q_hat
+            q_hat.backward()
+            with torch.no_grad():
+                self.w += self.alpha * delta * self.w.grad 
+                self.w.grad.zero_()
+            self.training_error.append(delta.detach().numpy())
+            if terminated or truncated:
+                self.decay_epsilon()
+                state, _ = self.env.reset()
+            else:
+                state = s_prime
+    
+    def sarsa(self, max_iterations: int = 1_000):
+        """Performs SARSA training
+
+        Args:
+            max_iterations (int, optional): Maximum number of iterations. Defaults to 1_000.
+        """
+        state, _ = self.env.reset()
+        for i in tqdm(range(max_iterations)):
+            action = self.e_greedy_policy(state)
+            s_prime, reward, terminated, truncated, _ = self.env.step(action)
+            a_prime = self.e_greedy_policy(s_prime)
+            target = reward + self.gamma * self.q(s_prime, a_prime)
+            q_hat = self.q(state, action)
+            delta = target - q_hat
+            q_hat.backward()
+            with torch.no_grad():
+                self.w += self.alpha * delta * self.w.grad 
+                self.w.grad.zero_()
+            self.training_error.append(delta.detach().numpy())
+            if terminated or truncated:
+                self.decay_epsilon()
+                state, _ = self.env.reset()
+            else:
+                state = s_prime
+
     def decay_epsilon(self):
         """Reduce epsilon for e-greedy policies"""
         self.epsilon = max(self.e_final, self.epsilon - self.e_decay)
--- 2023-24/gymp/gymp/mdp/frozenlake.py
+++ 2023-24/gymp/gymp/mdp/frozenlake.py
@@ -1,26 +1,39 @@
-from agents import QLearningAgent
 import gymnasium as gym
 from tqdm import tqdm 
 import numpy as np 
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import pandas as pd
+import torch
+from agents import AgentQFA
+
 sns.set_theme(style="whitegrid")
+
+class QLearningAgent(AgentQFA):
+    def x(self, state, action):
+        # one hot encoded actions
+        av = [1 if a == action else 0 for a in range(self.env.action_space.n)]
+        # one hot encoded states
+        st = [0]*self.env.observation_space.n
+        st[state] = 1
+        f = np.array(st + av)
+        return torch.tensor(f, dtype=float, requires_grad=False)
 
 
 ## Initialize
 env = gym.make("FrozenLake-v1", render_mode=None)
-learning_rate = 0.001
-n_episodes = 10_000
+learning_rate = 0.1
+n_episodes = 100_000
 start_epsilon = 1.0
 epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
-final_epsilon = 0.1
+final_epsilon = 0.05
 
 env = gym.wrappers.RecordEpisodeStatistics(env, deque_size=n_episodes)
 
 ## Qlearning
 agent = QLearningAgent(
-    environment=env, learning_rate=learning_rate, epsilon=start_epsilon, epsilon_decay=epsilon_decay, final_epsilon=final_epsilon, gamma=.95
+    n_features=env.observation_space.n + env.action_space.n, environment=env, learning_rate=learning_rate, epsilon=start_epsilon, 
+    epsilon_decay=epsilon_decay, final_epsilon=final_epsilon, gamma=.95
 )
 
 ## Execute episode without training
@@ -37,18 +50,7 @@ for episode in tqdm(range(5)):
         state = s_prime
 
 ## Training
-for episode in tqdm(range(n_episodes)):
-    state, info = env.reset()
-    done = False
-    # episode run
-    while not done:
-        action = agent.e_greedy_policy(state)
-        s_prime, reward, terminated, truncated, info = env.step(action=action)
-        # update
-        agent.update(state, action, reward, terminated, s_prime)
-        done = terminated or truncated
-        state = s_prime
-    agent.decay_epsilon()
+agent.q_learning(max_iterations=n_episodes)
         
 ## Execute episode
 testenv2 = gym.make("FrozenLake-v1", render_mode="human")
@@ -58,23 +60,18 @@ for episode in tqdm(range(10)):
     done = False
     # episode run
     while not done:
-        action = agent.greedy_policy(state)
+        action = agent.e_greedy_policy(state)
         s_prime, reward, terminated, truncated, info = testenv2.step(action=action)
         done = terminated or truncated
         state = s_prime
 
 
-space = np.zeros(len(agent.Q))
-for s, values in agent.Q.items():
-    v = values.max()
-    print(space[s])
-    space[s] = v 
-    print(space[s])
-V = space.reshape(int(np.sqrt(len(agent.Q))), -1)
-
+qa = np.zeros((env.observation_space.n, env.action_space.n))
+for s in range(env.observation_space.n):
+    for a in range(env.action_space.n):
+        qa[s, a] = agent.q(s, a)
+        
 fig, ax = plt.subplots(figsize=(4,3))
-sns.heatmap(V, ax=ax, annot=True, fmt=".3e", cmap='crest')
+sns.heatmap(qa, ax=ax, annot=True, fmt=".3e", cmap='crest')
 plt.tight_layout()
 plt.show()
-
-print(pd.DataFrame(V))
